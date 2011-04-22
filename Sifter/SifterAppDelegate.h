482cc418de5b0e5a3e3259b8ef3baa36fcc6c4dd
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-04-21 21:05:39 -0500

    Initial Commit

---
+++ Sifter/SifterAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SifterAppDelegate.h
+//  Sifter
+//
+//  Created by Bryan Mikaelian on 4/21/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SifterAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
