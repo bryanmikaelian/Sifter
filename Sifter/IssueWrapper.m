02274bcad402817642bc54969a715020c62d5aee
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-04-26 08:18:51 -0500

    Missing Files

---
+++ Sifter/IssueWrapper.m
@@ -0,0 +1,39 @@
+//
+//  IssueWrapper.m
+//  Sifter
+//
+//  Created by Bryan Mikaelian on 4/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "IssueWrapper.h"
+
+
+@implementation IssueWrapper
+
+@synthesize issueSubject;
+@synthesize issueNumber;
+@synthesize issueDescription;
+@synthesize issueURL;
+
+- (IssueWrapper*)initWithIssue:(NSDictionary *)issue {
+    if((self = [super init])) {
+        // Create an issue wrapper.  Add the following values of the issue as properties of this wrapper.  We can add more properties as needed.
+        self.issueSubject = [issue valueForKey:@"subject"];
+        self.issueNumber = [[issue valueForKey:@"number"] stringValue];
+        self.issueDescription = [issue valueForKey:@"description"];
+        self.issueURL = [issue valueForKey:@"url"];
+    }
+    
+    return self;
+}
+
+- (void)dealloc {
+    [issueSubject release];
+    [issueNumber release];
+    [issueDescription release];
+    [issueURL release];
+    [super dealloc];
+}
+
+@end
