1d0bd133ef4f9737d065a401f263c0a17836d8a3
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-04-26 07:11:19 -0500

    Added the class IssueCell.  This will be used to render the cells that are shown when the user taps "All Issues" or taps a specific milestone to view its issues.

---
+++ Sifter/IssueCell.h
@@ -0,0 +1,16 @@
+//
+//  IssueCell.h
+//  Sifter
+//
+//  Created by Bryan Mikaelian on 4/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface IssueCell : UITableViewCell {
+    
+}
+
+@end
