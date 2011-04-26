c2c2a48846955241925d02bc7a85a165bac7e974
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-04-26 08:18:25 -0500

    Missing files

---
+++ Sifter/IssueWrapper.h
@@ -0,0 +1,26 @@
+//
+//  IssueWrapper.h
+//  Sifter
+//
+//  Created by Bryan Mikaelian on 4/26/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IssueWrapper : NSObject {
+    NSString *issueNumber;
+    NSString *issueSubject;
+    NSString *issueDescription;
+    NSString *issueURL;
+}
+
+@property (nonatomic, retain) NSString *issueNumber;
+@property (nonatomic, retain) NSString *issueSubject;
+@property (nonatomic, retain) NSString *issueDescription;
+@property (nonatomic, retain) NSString *issueURL;
+
+- (IssueWrapper*) initWithIssue:(NSDictionary *)issue;
+
+@end
