ccc6ab5626df78dfaa497ba2c7ccfc4b3ef1dedb
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-04-26 13:23:31 -0500

    Cleaned up how we draw the issue cells to handle multiple lines and dynamic sizes.

--- Sifter/IssueView.m
+++ Sifter/IssueView.m
@@ -25,30 +25,46 @@
 
 
 - (void)drawRect:(CGRect)rect {
+#define LEFT_OFFSET 10
+#define CONTENT_WIDTH 275
+#define VERTICAL_OFFSET 6
+#define DESCRIPTION_OFFSET 24
     
     // Color and font for the issue subject and number
     UIFont *issueInfoFont = [UIFont boldSystemFontOfSize:14.0];
-    CGPoint infoPoint, descriptionPoint;
+    CGPoint subjectPoint, descriptionPoint;
     
     // Color and font for the issue description
-    UIFont *issueDescription = [UIFont systemFontOfSize:12.0];
+    UIFont *issueDescriptionFont = [UIFont systemFontOfSize:12.0];
     
     // Draw the subject
-    infoPoint = CGPointMake(10.0, 8.0);
-    [issueWrapper.issueSubject drawAtPoint:infoPoint forWidth:250.0 withFont:issueInfoFont 
-                               minFontSize:14.0 
-                               actualFontSize:NULL 
-                               lineBreakMode:UILineBreakModeWordWrap 
-                               baselineAdjustment:UIBaselineAdjustmentAlignBaselines];
     
+    // Specify the point where we want to place the info
+    subjectPoint = CGPointMake(LEFT_OFFSET, VERTICAL_OFFSET);
+    
+    // Because the subject can be several lines, we will need to figure out the size needed for the height of the rectangle that will hold this text.
+    CGSize boundingSize = CGSizeMake(CONTENT_WIDTH, CGFLOAT_MAX);  // Ideally we want all the space!
+    
+    // Set the size to be equal to the font, with the constaint set to the bounding size.  Issue subjects can never exceed 200 characters, so three lines would be used at most.
+    CGSize requiredSize = [issueWrapper.issueSubject sizeWithFont:issueInfoFont constrainedToSize:boundingSize lineBreakMode:UILineBreakModeWordWrap];
+
+    // Create a rectangle to hold this text.  The height is equal to the required size.
+    CGRect infoRectangle = CGRectMake(subjectPoint.x, subjectPoint.y, CONTENT_WIDTH, requiredSize.height);
+    [issueWrapper.issueSubject drawInRect:infoRectangle withFont:issueInfoFont lineBreakMode:UILineBreakModeWordWrap];
+        
     // Draw the description
-    descriptionPoint = CGPointMake(10.0, 24.0);
-    [issueWrapper.issueDescription drawAtPoint:descriptionPoint forWidth:250.0 withFont:issueDescription 
-                                   minFontSize:14.0 
-                                   actualFontSize:NULL 
-                                   lineBreakMode:UILineBreakModeWordWrap 
-                                   baselineAdjustment:UIBaselineAdjustmentAlignBaselines];
     
+    // Because the subject may take multiple lines, we can only allocate so much space for the description.  The point we will draw at will be right below the last line of the subject.
+    // Create the drawing point for the rectangle that will hold the content. Add 6 pixels for buffer so the text isn't on top of each other.
+    descriptionPoint = CGPointMake(LEFT_OFFSET, requiredSize.height + VERTICAL_OFFSET);
+    
+    // Figure out how much space is left for the description.
+    CGSize remainingSize = CGSizeMake(CONTENT_WIDTH, requiredSize.height + DESCRIPTION_OFFSET);   
+    CGSize requiredSizeForDescription = [issueWrapper.issueDescription sizeWithFont:issueDescriptionFont constrainedToSize:remainingSize lineBreakMode:UILineBreakModeTailTruncation];
+    
+    // Draw the rectangle to hold the text for the description.  This rectangle's height will be the height of the left over space. 
+    CGRect descriptionRectangle = CGRectMake(descriptionPoint.x, descriptionPoint.y, CONTENT_WIDTH, requiredSizeForDescription.height);
+    [issueWrapper.issueDescription drawInRect:descriptionRectangle withFont:issueDescriptionFont lineBreakMode:UILineBreakModeTailTruncation];
     
 }
 
