f7f0a0259750c2102ec3657ccdf333647256cfca
Bryan Mikaelian <bryan.mikaelian@gmail.com>
2011-05-03 15:32:39 -0500

    Added the much needed unit test target to the project

---
+++ SifterTest/SifterTest.h
@@ -0,0 +1,17 @@
+//
+//  SifterTest.h
+//  SifterTest
+//
+//  Created by Bryan Mikaelian on 5/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+
+@interface SifterTest : SenTestCase {
+@private
+    
+}
+
+@end
---
+++ SifterTest/SifterTest.m
@@ -0,0 +1,32 @@
+//
+//  SifterTest.m
+//  SifterTest
+//
+//  Created by Bryan Mikaelian on 5/3/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "SifterTest.h"
+
+
+@implementation SifterTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample {
+    STAssertEquals(1, 1, @"1 equals 1 :D");
+}
+
+@end
